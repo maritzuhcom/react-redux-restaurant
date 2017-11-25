a1dc29dd16d488141755968a0ce020615cc54d7e
Maritza Mancillas <mancillas.maritza@gmail.com>
2017-11-24 20:24:54 -0800

    Contact is done

--- src/components/screens/Contact.js
+++ src/components/screens/Contact.js
@@ -1,8 +1,103 @@
 import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 
 export default class Contact extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      nameError: '',
+      email: '',
+      emailError: '',
+      subject: '',
+      subjectError: '',
+      message: '',
+      messageError: '',
+      modalOpen: false
+    }
+  }
+
+
+  onChangeName = (e) => {
+    this.setState({
+      name: e.target.value
+    });
+  }
+
+  onChangeEmail = (e) => {
+    this.setState({
+      email: e.target.value
+    });
+  }
+
+  onChangeSubject = (e) => {
+    this.setState({
+      subject: e.target.value
+    });
+  }
+
+  onChangeMessage = (e) => {
+    this.setState({
+      message: e.target.value
+    });
+  }
+
+  emailIsValid = () => {
+    const { email } = this.state;
+
+    if(!email.includes('@')) {
+      return false;
+    }
+
+
+    if(!email.includes('.')) {
+      return false;
+    }
+
+    return true;
+  }
+
+  handleOpen = () => {
+    const errorMessage = 'This field is required'
+
+    if (this.state.name === '' ||
+      !this.emailIsValid() ||
+      this.state.email === '' ||
+      this.state.subject === '' ||
+      this.state.message === ''
+    ) {
+
+      this.state.name === '' ? this.setState({nameError: errorMessage}) : '';
+      this.emailIsValid() ? '' : this.setState({emailError: 'Use a correct email'});
+      this.state.subject === '' ? this.setState({subjectError: errorMessage}) : '';
+      this.state.message === '' ? this.setState({messageError: errorMessage}) : '';
+
+      return;
+    }
+
+    this.setState({
+      modalOpen: true,
+      name: '',
+      nameError: '',
+      email: '',
+      emailError: '',
+      subject: '',
+      subjectError: '',
+      message: '',
+      messageError: ''
+    });
+  }
+
+  handleClose = () => {
+    this.setState({
+      modalOpen: false
+    });
+  }
+
   render() {
     return (
       <main id='contact' style={Styles.contact}>
@@ -13,22 +108,30 @@ export default class Contact extends Component {
           <section style={Styles.fieldWrapper}>
             <TextField
               hintText='Name'
-              errorText='This field is required'
+              value={this.state.name}
+              errorText={this.state.nameError}
+              onChange={this.onChangeName}
             />
             <TextField
               hintText='Email'
-              errorText='This field is required'
+              value={this.state.email}
+              errorText={this.state.emailError}
+              onChange={this.onChangeEmail}
             />
           </section>
           <section style={Styles.fieldWrapper}>
             <TextField
               hintText='Subject'
-              errorText='This field is required'
+              value={this.state.subject}
+              errorText={this.state.subjectError}
+              onChange={this.onChangeSubject}
             />
             <TextField
               hintText='Message'
-              errorText='This field is required.'
+              value={this.state.message}
+              errorText={this.state.messageError}
               floatingLabelText='Message'
+              onChange={this.onChangeMessage}
               multiLine={true}
               rows={5}
               rowsMax={5}
@@ -39,7 +142,21 @@ export default class Contact extends Component {
         <RaisedButton
           label="Submit"
           fullWidth={true}
-        />
+          onClick={this.handleOpen}
+          />
+        <Dialog
+          actions={[
+            <FlatButton
+              label="Close"
+              primary={true}
+              onClick={this.handleClose}
+            />
+          ]}
+            open={this.state.modalOpen}
+            onRequestClose={this.handleClose}
+        >
+          Submitted
+        </Dialog>
       </main>
     );
   }
